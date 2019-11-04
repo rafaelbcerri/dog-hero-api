0e215aa0e202e140cbc921f8ebb0cbf012e1df47
Rafael Cerri <rafaelbcerri@gmail.com>
2019-11-03 22:15:12 -0300

    Remove puts

--- api/app/controllers/users_controller.rb
+++ api/app/controllers/users_controller.rb
@@ -7,10 +7,8 @@ class UsersController < ApplicationController
     user.name = update_params['name'] if update_params['name']
     user.img = update_params['img'] if update_params['img']
 
-    puts update_params.inspect
     if (update_params['dogs'])
       Dog.create(update_params['dogs']) do |dog|
-        puts dog
         dog.user = user
       end
     end
