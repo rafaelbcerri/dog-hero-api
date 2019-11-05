8bdba01e9441a7ddf017a3c2135120e233865767
Rafael Cerri <rafaelbcerri@gmail.com>
2019-11-04 23:24:21 -0300

    Create DogWalkingStatus model

---
+++ api/app/models/dog_walking_status.rb
@@ -0,0 +1,3 @@
+class DogWalkingStatus < ApplicationRecord
+  has_many :dog_walkings
+end
---
+++ api/test/models/dog_walking_status_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class DogWalkingStatusTest < ActiveSupport::TestCase
+  # test "the truth" do
+  #   assert true
+  # end
+end
