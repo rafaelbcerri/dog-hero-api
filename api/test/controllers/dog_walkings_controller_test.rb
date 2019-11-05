e2a73ac3dda9635dc92c1a8ea0fd295f1d79a029
Rafael Cerri <rafaelbcerri@gmail.com>
2019-11-05 01:21:41 -0300

    remove rails default tests

--- api/test/controllers/dog_walkings_controller_test.rb
+++
@@ -1,7 +0,0 @@
-require 'test_helper'
-
-class DogWalkingsControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
-end
--- api/test/models/dog_walking_status_test.rb
+++
@@ -1,7 +0,0 @@
-require 'test_helper'
-
-class DogWalkingStatusTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
-end
