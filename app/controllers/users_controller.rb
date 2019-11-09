c1381c88e5f3a83316c5ce6ad4c4d8c35be76af0
Rafael Cerri <rafaelbcerri@gmail.com>
2019-11-09 16:37:20 -0300

    Split method to pet owner and walker

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,11 +1,19 @@
 class UsersController < ApplicationController
   before_action :authenticate_user!
 
-  def get_dog_walkings
+  def get_pet_owner_dog_walkings
     dog_walkings = DogWalking
       .joins(:dogs)
       .where(:dogs => { user_id:  params[:id] })
       .group(:id)
+
+    render json: dog_walkings,
+           include: [:dogs, :dog_walking_status, :user]
+  end
+
+  def get_walker_dog_walkings
+    dog_walkings = DogWalking
+      .where(user_id: params[:id])
 
     render json: dog_walkings,
            include: [:dogs, :dog_walking_status, :user]
