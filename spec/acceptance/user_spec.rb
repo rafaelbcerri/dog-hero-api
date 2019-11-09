48bd8417b2a96f407936f8e1280f4c2680b4f990
Rafael Cerri <rafaelbcerri@gmail.com>
2019-11-09 16:39:50 -0300

    Create acceptance test for /walker/:id/dog-walkings

--- spec/acceptance/user_spec.rb
+++ spec/acceptance/user_spec.rb
@@ -32,4 +32,30 @@ resource 'User' do
       end
     end
   end
+
+  get '/walker/:id/dog-walkings' do
+    let(:user) { User.find_by_id(1) }
+    let(:api_key) { 'Bearer ' + Warden::JWTAuth::UserEncoder.new.call(user, :user, nil).first }
+    let(:id) { user.id }
+
+    context '200' do
+      example_request 'GET - All dog walkings from walker' do
+        expect(status).to eq 200
+
+        resp = JSON.parse(response_body)
+        expect(resp.length).to eq 3
+        expect(resp.first).to include(
+          'duration' => 30,
+          'latitude' => '-23.534296',
+          'longitude' => '-46.724113',
+          'begin_date' => '2019-11-09T16:32:00.000Z',
+          'end_date' => '2019-11-09T17:01:00.000Z',
+          'scheduled_date' => '2019-11-09T16:30:00.000Z',
+          'price' => 40.0,
+          'user_id' => 1,
+          'dog_walking_status_id' => 4
+        )
+      end
+    end
+  end
 end
